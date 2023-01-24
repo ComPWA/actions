cfef937af3915c3cce617a13261cd0fe8d4d2b8a
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2023-01-24 15:46:55 +0100

    FIX: refactor `joinByString()` function

--- create-pytest-matrix/get-json-python-versions.sh
+++ create-pytest-matrix/get-json-python-versions.sh
@@ -1,10 +1,14 @@
-function to_json_list() {
-  local separator=' <->'
-  result=$(printf "%s" "$separator" "${@/#/$separator}")
-  echo '["'${result}'"]'
+function joinByString() {
+  # https://dev.to/meleu/how-to-join-array-elements-in-a-bash-script-303a
+  local separator="$1"
+  shift
+  local first="$1"
+  shift
+  printf "%s" "$first" "${@/#/$separator}"
 }
 
+SEPARATOR='", "'
 PYTHON_VERSIONS="$@"
-JSON="$(to_json_list ${PYTHON_VERSIONS}})"
+JSON="$(joinByString ${SEPARATOR} ${PYTHON_VERSIONS}})"
 echo "PYTHON_VERSIONS=${JSON}"
 echo "{PYTHON_VERSIONS}=${JSON}" >>$GITHUB_ENV
