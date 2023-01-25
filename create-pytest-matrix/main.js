acb7caaf05c77e0b42b0c4af3ec9a0ee65eb0146
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2023-01-25 11:28:12 +0100

    Revert "MAINT: switch to javascript"
    This reverts commit b36c3740ecbb94a32cdd02ad657e737fa7621a62.

--- create-pytest-matrix/main.js
+++
@@ -1,9 +0,0 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
-
-try {
-  const pythonVersions = core.getInput("python-versions");
-  console.log(`Python versions: ${pythonVersions}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
