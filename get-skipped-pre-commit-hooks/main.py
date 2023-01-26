d62c143d007f1443023986b4742e75fbcbdfb277
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2023-01-26 15:49:56 +0100

    FEAT: run all pre-commit hooks if no pre-commit.ci

--- get-skipped-pre-commit-hooks/main.py
+++ get-skipped-pre-commit-hooks/main.py
@@ -21,7 +21,10 @@ def get_skipped_precommit_hooks() -> list[str]:
         raise FileExistsError(f"This repository does not have a {config_path}")
     with open(config_path) as f:
         config: dict[str, dict] = yaml.safe_load(f)
-    skipped_hooks = config.get("ci", {}).get("skip", [])
+    precommit_ci_config = config.get("ci")
+    if precommit_ci_config is None:
+        return ["ALL"]
+    skipped_hooks = precommit_ci_config.get("skip", [])
     return skipped_hooks
 
 
