ce6382793ca4485ca295c1078678c2bd5352541d
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2023-01-26 15:01:45 +0100

    FEAT: automatically determine skipped pre-commit hooks

---
+++ get-skipped-pre-commit-hooks/main.py
@@ -0,0 +1,29 @@
+"""Print job matrix for a GitHub Actions workflow that runs `pytest`."""
+
+from __future__ import annotations
+
+import json
+import os
+from configparser import ConfigParser
+
+import yaml
+
+
+def main() -> int:
+    skipped_hooks = get_skipped_precommit_hooks()
+    print(" ".join(skipped_hooks))
+    return 0
+
+
+def get_skipped_precommit_hooks() -> list[str]:
+    config_path = ".pre-commit-config.yaml"
+    if not os.path.exists(config_path):
+        raise FileExistsError(f"This repository does not have a {config_path}")
+    with open(config_path) as f:
+        config: dict[str, dict] = yaml.safe_load(f)
+    skipped_hooks = config.get("ci", {}).get("skip", [])
+    return skipped_hooks
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
