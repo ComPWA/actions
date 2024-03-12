d7da53f466c7ddf2dbae85416faeca943bc9693f
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2024-03-12 12:01:18 +0100

    ENH: determine package name automatically (#67)

---
+++ get-pypi-name/main.py
@@ -0,0 +1,40 @@
+"""Print job matrix for a GitHub Actions workflow that runs `pytest`."""
+
+from __future__ import annotations
+
+import os
+from configparser import ConfigParser
+
+import tomlkit
+
+
+def main() -> int:
+    for getter in (
+        _get_package_name_setup_cfg,
+        _get_package_name_pyproject_toml,
+    ):
+        package_name = getter()
+        if package_name is not None:
+            print(package_name)
+            return 0
+    return 1
+
+
+def _get_package_name_setup_cfg() -> str | None:
+    if not os.path.exists("setup.cfg"):
+        return None
+    cfg = ConfigParser()
+    cfg.read("setup.cfg")
+    return cfg.get("metadata", "name", fallback=None)
+
+
+def _get_package_name_pyproject_toml() -> str | None:
+    if not os.path.exists("pyproject.toml"):
+        return None
+    with open("pyproject.toml") as f:
+        pyproject = tomlkit.load(f)
+    return pyproject.get("project", {}).get("name")
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
