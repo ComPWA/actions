b36d0a12f63cf2e7410ab41d191074d8288d4b80
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2024-10-14 10:52:06 +0200

    FIX: determine allowed Python versions from `requires-python`

--- create-python-version-matrix/main.py
+++ create-python-version-matrix/main.py
@@ -1,17 +1,22 @@
 """Print job matrix for a GitHub Actions workflow that runs `pytest`."""
+# /// script
+# dependencies = [
+#   "packaging",
+# ]
+# requires-python = ">=3.12"
+# ///
 
 import json
 import os
 import tomllib
 from configparser import ConfigParser
+from dataclasses import dataclass
+
+from packaging.specifiers import SpecifierSet
+from packaging.version import Version
 
 PYPROJECT_TOML = "pyproject.toml"
 SETUP_CFG = "setup.cfg"
-
-CLASSIFIERS_ERROR_MSG = (
-    "This package does not have Python version classifiers, so cannot determine"
-    " intended Python versions. See https://pypi.org/classifiers."
-)
 VERSION_IDENTIFIER = "Programming Language :: Python :: 3."
 
 
@@ -28,47 +33,90 @@ def create_job_matrix() -> dict:
 
 
 def get_supported_python_versions() -> list[str]:
-    classifiers = _get_classifiers()
-    return _determine_python_versions(classifiers)
+    version_info = _get_version_info()
+    return _determine_python_versions(version_info)
 
 
-def _get_classifiers() -> list[str]:
+@dataclass
+class VersionInfo:
+    classifiers: list[str] | None
+    requires_python: str | None
+
+
+def _get_version_info() -> VersionInfo:
     if os.path.exists(SETUP_CFG):
-        return __get_classifiers_from_cfg(SETUP_CFG)
+        return _get_version_info_from_cfg(SETUP_CFG)
     if os.path.exists(PYPROJECT_TOML):
-        return __get_classifiers_from_toml(PYPROJECT_TOML)
+        return _get_version_info_from_toml(PYPROJECT_TOML)
     msg = f"This project does not contain a {SETUP_CFG} or {PYPROJECT_TOML}"
     raise FileNotFoundError(msg)
 
 
-def __get_classifiers_from_cfg(path: str) -> list[str]:
+def _get_version_info_from_cfg(path: str) -> VersionInfo:
     cfg = ConfigParser()
     cfg.read(path)
-    if not cfg.has_option("metadata", "classifiers"):
-        raise ValueError(CLASSIFIERS_ERROR_MSG)
+    return VersionInfo(
+        classifiers=__get(cfg, "metadata", "classifiers", typ=list),
+        requires_python=__get(cfg, "options", "python_requires"),
+    )
+
+
+def __get[T](
+    cfg: ConfigParser, section: str, option: str, typ: type[T] = str
+) -> T | None:
+    if not cfg.has_option(section, option):
+        return None
     raw = cfg.get("metadata", "classifiers")
-    return [s.strip() for s in raw.split("\n") if s.strip()]
+    if typ is str:
+        return raw  # pyright: ignore[reportReturnType]
+    if typ is list:
+        return [s.strip() for s in raw.split("\n") if s.strip()]  # pyright: ignore[reportReturnType]
+    msg = f"Unsupported cast type: {typ}"
+    raise NotImplementedError(msg)
 
 
-def __get_classifiers_from_toml(path: str) -> list[str]:
+def _get_version_info_from_toml(path: str) -> VersionInfo:
     with open(path, "rb") as f:
         cfg = tomllib.load(f)
-    classifiers = cfg.get("project", {}).get("classifiers")
-    if classifiers is None:
-        raise ValueError(CLASSIFIERS_ERROR_MSG)
-    return classifiers
+    project_table: dict = cfg.get("project", {})
+    return VersionInfo(
+        classifiers=project_table.get("classifiers"),
+        requires_python=project_table.get("requires-python"),
+    )
 
 
-def _determine_python_versions(classifiers: list[str]) -> list[str]:
-    versions = [s for s in classifiers if s.startswith(VERSION_IDENTIFIER)]
+def _determine_python_versions(version_info: VersionInfo) -> list[str]:
+    supported_versions = _determine_python_versions_from_classifiers(version_info)
+    if supported_versions is not None:
+        return supported_versions
+    if version_info.requires_python is not None:
+        return __get_allowed_versions(version_info.requires_python)
+    msg = "No PyPI classifiers or minimal Python version defined"
+    raise ValueError(msg)
+
+
+def _determine_python_versions_from_classifiers(
+    version_info: VersionInfo,
+) -> list[str] | None:
+    if version_info.classifiers is None:
+        return None
+    versions = [s for s in version_info.classifiers if s.startswith(VERSION_IDENTIFIER)]
     if not versions:
-        msg = (
-            f"{SETUP_CFG} does not have any classifiers of the form"
-            f' "{VERSION_IDENTIFIER}*"'
-        )
-        raise ValueError(msg)
+        return None
     prefix = VERSION_IDENTIFIER[:-2]
     return [s.replace(prefix, "") for s in versions]
+
+
+def __get_allowed_versions(version_range: str) -> list[str]:
+    """Get a list of allowed versions from a version range specifier.
+
+    >>> __get_allowed_versions(">=3.9,<3.13")
+    ['3.9', '3.10', '3.11', '3.12']
+    """
+    specifier = SpecifierSet(version_range)
+    allowed_versions = [f"3.{v}" for v in range(6, 13)]
+    versions_to_check = [Version(v) for v in sorted(allowed_versions)]
+    return [str(v) for v in versions_to_check if v in specifier]
 
 
 if __name__ == "__main__":
