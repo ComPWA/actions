c4088f202ea0efd774f6ab7f358f620568ba215b
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2026-01-10 15:03:20 +0100

    BREAK: run tasks through `poe` or `pixi` only

--- create-pytest-matrix/main.py
+++ create-pytest-matrix/main.py
@@ -13,7 +13,7 @@ import os
 import tomllib
 from argparse import ArgumentParser
 from dataclasses import dataclass
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Literal
 
 from packaging.specifiers import SpecifierSet
 from packaging.version import Version
@@ -21,37 +21,38 @@ from packaging.version import Version
 if TYPE_CHECKING:
     from collections.abc import Sequence
 
+PYTHON_VERSION = Literal[
+    "3.6", "3.7", "3.8", "3.9", "3.10", "3.11", "3.12", "3.13", "3.14"
+]
+
 
 def main(argv: Sequence[str] | None = None) -> int:
     parser = ArgumentParser(__doc__)
-    parser.add_argument("coverage-python-version")
-    parser.add_argument("coverage-target")
-    parser.add_argument("macos-python-version")
-    parser.add_argument("skipped-python-versions")
+    parser.add_argument("--coverage-python-version", choices=PYTHON_VERSION.__args__)
+    parser.add_argument("--macos-python-version", choices=PYTHON_VERSION.__args__)
+    parser.add_argument("--skipped-python-versions", type=str)
     args = parser.parse_args(argv)
     matrix = create_job_matrix(
-        args.__dict__["coverage-python-version"],
-        args.__dict__["coverage-target"],
-        args.__dict__["macos-python-version"],
-        _format_skipped_version(args.__dict__["skipped-python-versions"]),
+        args.coverage_python_version,
+        args.macos_python_version,
+        _format_skipped_version(args.skipped_python_versions),
     )
     print(json.dumps(matrix, indent=2))
     return 0
 
 
-def _format_skipped_version(skipped_python_versions: str) -> set[str] | None:
+def _format_skipped_version(skipped_python_versions: str) -> set[PYTHON_VERSION] | None:
     if skipped_python_versions == "all":
         return None
     if not skipped_python_versions:
         return set()
-    return set(skipped_python_versions.split(" "))
+    return set(skipped_python_versions.split(" "))  # ty:ignore[invalid-return-type]
 
 
 def create_job_matrix(
-    coverage_python_version: str,
-    coverage_target: str,
-    macos_python_version: str,
-    skipped_python_versions: set[str] | None,
+    coverage_python_version: PYTHON_VERSION | Literal[""],
+    macos_python_version: PYTHON_VERSION,
+    skipped_python_versions: set[PYTHON_VERSION] | None,
 ) -> dict:
     supported_python_versions = get_supported_python_versions()
     if skipped_python_versions is None:
@@ -60,7 +61,7 @@ def create_job_matrix(
         python_versions = sorted(
             set(supported_python_versions) - skipped_python_versions
         )
-    if coverage_target:
+    if coverage_python_version:
         _is_version_allowed(coverage_python_version, supported_python_versions)
         if coverage_python_version in python_versions:
             python_versions.remove(coverage_python_version)
@@ -77,9 +78,9 @@ def create_job_matrix(
             "python-version": "3.7",
             "runs-on": "ubuntu-22.04",
         })
-    if coverage_target:
+    if coverage_python_version:
         includes.append({
-            "coverage-target": coverage_target,
+            "codecov": "codecov",
             "python-version": coverage_python_version,
             "runs-on": "ubuntu-24.04",
         })
@@ -100,7 +101,9 @@ def create_job_matrix(
     return matrix
 
 
-def _is_version_allowed(version: str, supported_versions: list[str]) -> None:
+def _is_version_allowed(
+    version: PYTHON_VERSION, supported_versions: list[PYTHON_VERSION]
+) -> None:
     if version not in supported_versions:
         supported_versions_str = ", ".join(supported_versions)
         msg = f"Selected Python {version}, but the package only supports Python {supported_versions_str}"
@@ -111,7 +114,7 @@ PYPROJECT_TOML = "pyproject.toml"
 VERSION_IDENTIFIER = "Programming Language :: Python :: 3."
 
 
-def get_supported_python_versions() -> list[str]:
+def get_supported_python_versions() -> list[PYTHON_VERSION]:
     version_info = _get_version_info()
     return _determine_python_versions(version_info)
 
@@ -136,7 +139,7 @@ def _get_version_info() -> VersionInfo:
     )
 
 
-def _determine_python_versions(version_info: VersionInfo) -> list[str]:
+def _determine_python_versions(version_info: VersionInfo) -> list[PYTHON_VERSION]:
     supported_versions = _determine_python_versions_from_classifiers(version_info)
     if supported_versions is not None:
         return supported_versions
@@ -148,7 +151,7 @@ def _determine_python_versions(version_info: VersionInfo) -> list[str]:
 
 def _determine_python_versions_from_classifiers(
     version_info: VersionInfo,
-) -> list[str] | None:
+) -> list[PYTHON_VERSION] | None:
     if version_info.classifiers is None:
         return None
     version_identifier = "Programming Language :: Python :: 3."
@@ -156,10 +159,10 @@ def _determine_python_versions_from_classifiers(
     if not versions:
         return None
     prefix = version_identifier[:-2]
-    return [s.replace(prefix, "") for s in versions]
+    return [s.replace(prefix, "") for s in versions]  # ty:ignore[invalid-return-type]
 
 
-def __get_allowed_versions(version_range: str) -> list[str]:
+def __get_allowed_versions(version_range: str) -> list[PYTHON_VERSION]:
     """Get a list of allowed versions from a version range specifier.
 
     >>> __get_allowed_versions(">=3.9,<3.13")
@@ -168,7 +171,7 @@ def __get_allowed_versions(version_range: str) -> list[str]:
     specifier = SpecifierSet(version_range)
     allowed_versions = [f"3.{v}" for v in range(6, 15)]
     versions_to_check = [Version(v) for v in sorted(allowed_versions)]
-    return [str(v) for v in versions_to_check if v in specifier]
+    return [str(v) for v in versions_to_check if v in specifier]  # ty:ignore[invalid-return-type]
 
 
 if __name__ == "__main__":
